ace2a09cc19794458c1e4d5ead695b0e05a3b2f3
AichaAO97 <aicha.ait.oumghar17@gmail.com>
2023-12-05 19:20:32 +0100

    send email after signing up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,8 @@
 const bcrypt = require('bcryptjs');
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY || 'a89f75ad7ff5d35d245a06b3c0d3bc1b-0a688b4a-2dc422bf'});
 
 const User = require('../models/user');
 
@@ -89,6 +93,16 @@ exports.postSignup = (req, res, next) => {
     })
     .then(result => {
       res.redirect('/login');
+      // Sending a welcome email using Mailgun
+      mg.messages.create('sandbox2e0f5587af5d487db2192fb8c995f89f.mailgun.org', {
+        from: 'The shop app <shop@node-complete.com>',
+        to: [email],
+        subject: 'Signup succeeded!',
+        text: "Testing some Mailgun awesomeness!",
+        html: "<h1>You successfully signed up!</h1>"
+      })
+      .then(msg => console.log({msg})) // logs response data
+      .catch(err => console.log({err})); // logs any error
     })
     .catch(err => {
       console.log(err);
