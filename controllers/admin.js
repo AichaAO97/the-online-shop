6ea9d3e1f290a8046f99a56abae2905092167200
AichaAO97 <aicha.ait.oumghar17@gmail.com>
2023-12-06 20:54:38 +0100

    add authorization to the app to restrict access and actions

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,13 +16,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product({title, price, description, imageUrl, userId: req.user}); // or userId: req.user._id
+  const product = new Product({ title, price, description, imageUrl, userId: req.user }); // or userId: req.user._id
 
   product.save()
     .then(result => {
-        console.log('Created product');
-        res.redirect('/admin/products');
-      })
+      console.log('Created product');
+      res.redirect('/admin/products');
+    })
     .catch(err => {
       console.log(err);
     });
@@ -30,7 +30,7 @@ exports.postAddProduct = (req, res, next) => {
 
 // this works as it is thanks to God, then to mongoose
 exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit ; 
+  const editMode = req.query.edit;
   if (!editMode) {
     return res.redirect('/');
   }
@@ -61,26 +61,29 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedDesc = req.body.description;
   Product.findById(prodId)
-  .then(product => {
-    product.title = updatedTitle;
-    product.price = updatedPrice;
-    product.imageUrl = updatedImageUrl;
-    product.description = updatedDesc;
-    return product.save();
-  })
-  .then(result => {
-    console.log("UPDATED PRODUCT");
-    res.redirect('/admin/products');
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .then(product => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect('/');
+      }
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+      return product.save()
+        .then(result => {
+          console.log("UPDATED PRODUCT");
+          res.redirect('/admin/products');
+        });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 
 };
 
 //this is fixed
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  Product.find({ userId: req.user._id })
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -96,10 +99,10 @@ exports.getProducts = (req, res, next) => {
 //this is fixed
 exports.postDeleteProduct = (req, res, next) => {
   const productId = req.body.productId;
-  Product.findByIdAndDelete(productId)
-    .then( (result) => {
-    console.log("DESTROYED PRODUCT");
-    res.redirect('/admin/products');
+  Product.deleteOne({ _id: productId, userId: req.user._id })
+    .then((result) => {
+      console.log("DESTROYED PRODUCT");
+      res.redirect('/admin/products');
     })
     .catch(err => console.log(err));
 };
